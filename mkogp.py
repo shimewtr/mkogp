7a71525518fda6ee0409ccbd505e8c2d61dc9983
ShimeWataru <shime@shimewtr.me>
2020-09-26 11:09:20 +0900

    mdファイルからタイトルと出力パスを取得するように変更

--- mkogp.py
+++ mkogp.py
@@ -1,17 +1,49 @@
+import glob
 import os
+import pathlib
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 EXPORT_PATH = os.environ['EXPORT_PATH']
+POSTS_PATH = os.environ['POSTS_PATH']
+
+# def extract_meta_data(file_path):
+
+
+def extract_meta_data(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+        indexes = [i for i, x in enumerate(lines) if x == '---\n']
+        return (lines[indexes[0] + 1:indexes[1]])
+
+
+def extract_title_and_ogp_name(meta_data):
+    title_prefix = 'title:'
+    ogp_prefix = 'ogp:'
+    title = [s for s in meta_data if s.startswith(title_prefix)][0].replace(
+        title_prefix, '').strip().strip('"')
+    ogp = [s for s in meta_data if s.startswith(ogp_prefix)][0].replace(
+        ogp_prefix, '').strip().strip('"')
+    return title, ogp
+
+
+def glob_md():
+    files = glob.glob(POSTS_PATH + '/*.md')
+    for file in files:
+        meta_data = extract_meta_data(file)
+        title, ogp_name = extract_title_and_ogp_name(meta_data)
+        mkogp(title, ogp_name)
+
 
 def mkdirs():
     os.makedirs(EXPORT_PATH, exist_ok=True)
 
-def mkogp():
+
+def mkogp(title, ogp):
     image_size = (1200, 630)
     icon_size = (160, 160)
     background_color = (112, 112, 112)
     text_color = (255, 255, 255)
-    text = '記事タイトルを入れてください'
+    text = title
     font_size = 60
     font_medium_path = './fonts/MPLUS1p-Medium.ttf'
     sub_text = "ブログのタイトルを入れてください"
@@ -27,13 +59,13 @@ def mkogp():
              text_color, image_size[0]//2, image_size[1]//6 * 5)
     add_icon(im, image_size, icon_size, icon_path)
 
-    im.save(EXPORT_PATH + '/image.png')
+    im.save(EXPORT_PATH + '/' + ogp + '.png')
 
 
 def add_icon(im, image_size, icon_size, icon_path):
     icon = Image.open(icon_path).copy()
     icon = icon.resize(size=icon_size, resample=Image.ANTIALIAS)
-    mask = Image.new("L", icon.size, 0)
+    mask = Image.new('L', icon.size, 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0, icon.size[0], icon.size[1]), fill=255)
     mask = mask.filter(ImageFilter.GaussianBlur(1))
@@ -52,4 +84,4 @@ def add_text(im, text, font_size, fontPath, text_color, width, height):
 
 if __name__ == '__main__':
     mkdirs()
-    mkogp()
+    glob_md()
